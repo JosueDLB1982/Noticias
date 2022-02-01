d5a8c88474146f47c1e2476ad2bc9551f4eb9856
Josue_Lopez <josuedlopez1982@gmail.com>
2022-02-01 12:17:51 -0300

    Creado componente <Header/>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Fragment } from "react"
+import Header from "./components/Header"
 
 function App() {
   return (
-    <h1>Noticias</h1>
+    <Fragment>
+      <Header
+        title='Buscador de Noticias'
+      />
+
+      <div className="container white">
+        <h1>Formulario Aquí</h1>
+      </div>
+    </Fragment>
   );
 }
 
